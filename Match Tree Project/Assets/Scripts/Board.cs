2891b293a2065cc5585f6bf7280c5db221d30e8a
ievutev <ievyte2@gmail.com>
2023-05-13 21:36:33 +0300

    Sutvarkytas paskutinio lygio užbaigimo kodas.
    Pridėta game_end scena. Ištestuotas veikimas

--- Match Tree Project/Assets/Scripts/Board.cs
+++ Match Tree Project/Assets/Scripts/Board.cs
@@ -22,7 +22,7 @@ public sealed class Board : MonoBehaviour
     
     public Tile[,] Tiles { get; private set; }
     // Points, Pops, Moves, TimeValue           1                   2                   3                   4                   5                   6                  7                   8                   9                 10
-    public int[,] Goals = new int[,]{ { 100, -1, -1, -1}, { 200, -1, -1, 180}, { 300, -1, -1, -1}, { 100, -1, 10, -1}, { 500, -1, -1, -1}, { -1, 50, -1, 60}, { 500, -1, -1, 300}, { -1, 100, -1, 60}, { 500, 150, -1, -1}, { -1, 200, -1, 120} };
+    public int[,] Goals = new int[,]{ { 20, -1, -1, -1}, { 20, -1, -1, 180}, { 30, -1, -1, -1}, { 10, -1, 10, -1}, { 50, -1, -1, -1}, { -1, 10, -1, 60}, { 20, -1, -1, 300}, { -1, 10, -1, 60}, { 20, 10, -1, -1}, { -1, 20, -1, 120} };
 
     private int score = 0;
     private int gemsPoppedCount = 0;
@@ -78,7 +78,7 @@ public sealed class Board : MonoBehaviour
     private void Update()
     {
         //Points, Pops, Moves, TimeValue              1                   2                   3                   4                 5                6               7                8                  9                 10
-        //public int[,] Goals = new int[,]{ { 100, -1, -1, -1}, { 200, -1, -1, 180}, { 300, -1, -1, -1}, { 100, -1, 10, -1}, { 500, -1, -1, -1}, { -1, 50, -1, 60}, { 500, 0, 0, 0}, { 100, 0, 0, 0}, { 500, 150, 0, 0}, { 200, 100, 0, 0} };
+        //public int[,] Goals = new int[,]{ { 100, -1, -1, -1}, { 200, -1, -1, 180}, { 300, -1, -1, -1}, { 100, -1, 10, -1}, { 500, -1, -1, -1}, { -1, 50, -1, 60}, { 500, -1, -1, 300}, { -1, 100, -1, 60}, { 500, 150, -1, -1}, { -1, 200, -1, 120} };
 
         //tikrinam, ar reikia laikmačio. Jei reikia - skaičiuojam ir atvaizduojam
         if (Goals[level, 3] > 0)
@@ -94,23 +94,30 @@ public sealed class Board : MonoBehaviour
         {
             if (maxMoves==0)
                 SceneManager.LoadScene(14);
-            else if (maxMoves>0 && score >= Goals[level,0])
+            else if (maxMoves>0 && score >= Goals[level,0] && level != 9)
                 SceneManager.LoadScene(13);
+            else if (maxMoves > 0 && score >= Goals[level, 0] && level == 9)
+                SceneManager.LoadScene(15);
         }
 
         //tikrinam, ar surinktas reikiamas kiekis tasku
         if (Goals[level, 0] > 0)
         {
-            if (score >= Goals[level, 0])
+            if (score >= Goals[level, 0] && level != 9)
                 SceneManager.LoadScene(13);
+            if (score >= Goals[level, 0] && level == 9)
+                SceneManager.LoadScene(15);
         }
 
         //tikrinam, ar reikia susprogdinti tam tikrą skaičių gems
         if (Goals[level, 1] > 0)
         {
-            if (gemsPoppedCount >= Goals[level,1])
+            if (gemsPoppedCount >= Goals[level,1] && level != 9)
                 SceneManager.LoadScene(13);
+            if (gemsPoppedCount >= Goals[level, 1] && level == 9)
+                SceneManager.LoadScene(15);
         }
+
     }
 
     public void DisplayTime(float time)
@@ -249,10 +256,11 @@ public sealed class Board : MonoBehaviour
                     inflateSequence.Join(connectedTile.icon.transform.DOScale(Vector3.one, TweenDuration));
                 }
 
-                await inflateSequence.Play().AsyncWaitForCompletion();
                 Vector3 screenCenter = new Vector3(0.5f, 0.5f, Camera.main.nearClipPlane);
                 Vector3 worldCenter = Camera.main.ViewportToWorldPoint(screenCenter);
                 AudioSource.PlayClipAtPoint(poppingSoundEffect, worldCenter, 0.2f);
+                await inflateSequence.Play().AsyncWaitForCompletion();
+                
             }
         }
 
