17eda992e4c638c4f81184e8347c4add962dd38b
Ieva <83116607+ievutev@users.noreply.github.com>
2023-05-31 22:26:47 +0300

    final update

--- Match Tree Project/Assets/Scripts/MainMenu.cs
+++ Match Tree Project/Assets/Scripts/MainMenu.cs
@@ -54,7 +54,112 @@ public class MainMenu : MonoBehaviour
         int[] a = ach.GetAchieved();
 
         int screenIndex = 0;
+/* day and night
+[Range(0.0f, 1.0f)]
+    public float time;
+    public float fullDayLength;
+    public float startTime = 0.4f;
+    private float timeRate;
+    public Vector3 noon;
 
+    [Header("Sun")]
+    public Light sun;
+    public Gradient sunColor;
+    public AnimationCurve sunIntensity;
+
+    [Header("Moon")]
+    public Light moon;
+    public Gradient moonColor;
+    public AnimationCurve moonIntensity;
+
+    [Header("Other Lighting")]
+    public AnimationCurve lightingIntensityMultiplier;
+    public AnimationCurve reflectionIntensityMultiplier;
+
+    private float particleEmissionMin = 10.0f;
+    private float particleEmissionMax = 150.0f;
+
+    [SerializeField]
+    private ParticleSystem fire;
+    private ParticleSystem.EmissionModule fireEmission;
+
+    [SerializeField]
+    private ParticleSystem smoke;
+    private ParticleSystem.EmissionModule smokeEmission;
+
+    [SerializeField]
+    private ParticleSystem rain;
+    private ParticleSystem.EmissionModule rainEmission;
+
+
+    private void Start()
+    {
+        timeRate = 1.0f / fullDayLength;
+        time = startTime;
+        fireEmission = fire.emission;
+        smokeEmission = smoke.emission;
+        rainEmission = rain.emission;
+    }
+
+    private void Update()
+    {
+        time += timeRate * Time.deltaTime;
+        if (time >= 1.0f)
+            time = 0.0f;
+
+        sun.transform.eulerAngles = (time - 0.25f) * noon * 4.0f;
+        moon.transform.eulerAngles = (time - 0.75f) * noon * 4.0f;
+
+        if (sun.intensity == 0 && sun.gameObject.activeInHierarchy)
+            sun.gameObject.SetActive(false);
+        else if(sun.intensity > 0 && !sun.gameObject.activeInHierarchy)
+            sun.gameObject.SetActive(true);
+
+        if (moon.intensity == 0 && moon.gameObject.activeInHierarchy)
+            moon.gameObject.SetActive(false);
+        else if (moon.intensity > 0 && !moon.gameObject.activeInHierarchy)
+            moon.gameObject.SetActive(true);
+
+        if (time > 0.0f && time < 0.5f)
+        {
+            fireEmission.rateOverTime = Mathf.Lerp(particleEmissionMin, particleEmissionMax, time / 0.5f);
+            smokeEmission.rateOverTime = Mathf.Lerp(particleEmissionMin, particleEmissionMax, time / 0.5f);
+        }
+        else
+        {
+            fireEmission.rateOverTime = Mathf.Lerp(particleEmissionMax, particleEmissionMin, time / 0.75f);
+            smokeEmission.rateOverTime = Mathf.Lerp(particleEmissionMax, particleEmissionMin, time / 0.75f);
+        }
+
+        if (time > 0.75f && time < 0.875f)
+        {
+            rainEmission.rateOverTime = Mathf.Lerp(particleEmissionMin, particleEmissionMax, time / 0.875f);
+        }
+        if (time > 0.875f && time < 1f)
+        {
+            rainEmission.rateOverTime = Mathf.Lerp(particleEmissionMax, particleEmissionMin, time / 1f);
+        }
+
+        if (time > 0.0f && time < 0.75f)
+        {
+            if (!fire.isPlaying)
+                fire.Play();
+            if (!smoke.isPlaying)
+                smoke.Play();
+            if (rain.isPlaying)
+                rain.Stop();
+        }
+        else
+        {
+            if (!rain.isPlaying)
+                rain.Play();
+            if (fire.isPlaying)
+                fire.Stop();
+            if (smoke.isPlaying)
+                smoke.Stop();
+        }
+    }
+*/
         if (level_select.gameObject.tag == "level1")
         {
             if (a[0] == 0)
